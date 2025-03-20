ade93b0ced00ee8a8073678c1cf6c8db742f6dfb
Vroksi Roy <royvroksi@gmail.com>
2025-03-20 12:02:19 +0530

    feat: add custom Outfit font support

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { Text } from "react-native";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    'outfit': require('../assets/fonts/Outfit-Regular.ttf'),
+    'outfit-bold': require('../assets/fonts/Outfit-Bold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>; // Show a loading message until fonts load
+  }
   return <Stack />;
 }
